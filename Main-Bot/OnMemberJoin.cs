32b75e43f68ad6d9f6949c3d5c063c96b6b0577f
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-11-16 22:47:20 +0100

    Completed OnMemberJoin feature

---
+++ Main-Bot/OnMemberJoin.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using NetCord.Hosting.Gateway;
+using NetCord.Rest;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Options;
+using NetCord;
+
+namespace Main_Bot;
+
+[UsedImplicitly]
+public class OnMemberJoin(IOptions<SN41Settings> settings) : IGuildUserAddGatewayHandler {
+    private static Stream GetImageStream() {
+        var assembly = Assembly.GetExecutingAssembly();
+        return assembly.GetManifestResourceStream("Main_Bot.welcome.png") ??
+               throw new InvalidOperationException("Missing welcome.png inside assembly resources");
+    }
+
+    public async ValueTask HandleAsync(GuildUser guildUser) {
+        await using var file = GetImageStream();
+        var image = new AttachmentProperties("image.png", file);
+        var body = String.Format(settings.Value.WelcomeMessage.Body, guildUser.Username);
+        var embed = new EmbedProperties()
+            .WithDescription(body)
+            .WithColor(new(0xF1C40F))
+            .WithImage("attachment://image.png");
+        var message = new MessageProperties()
+            .WithAttachments([image])
+            .WithEmbeds([embed]);
+        DMChannel channel = await guildUser.GetDMChannelAsync();
+        await channel.SendMessageAsync(message);
+    }
+}
--- Main-Bot/Program.cs
+++ Main-Bot/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Main_Bot;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using NetCord.Gateway;
 using NetCord.Hosting.Gateway;
 
@@ -6,6 +8,12 @@ var builder = Host.CreateApplicationBuilder(args);
 
 if (builder.Configuration["Discord:Token"] == null)
     throw new InvalidOperationException("Discord Token is not configured.");
+
+builder.Services
+    .AddOptions<SN41Settings>()
+    .Bind(builder.Configuration.GetSection("SN41"))
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
 
 builder.Services
     .AddDiscordGateway(options => { options.Intents = GatewayIntents.All; })
