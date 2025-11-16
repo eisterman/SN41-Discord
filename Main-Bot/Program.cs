3766a0cc9d6b125a2bb4ccadc77811511dec1556
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-11-16 21:02:20 +0100

    Initial Commit

---
+++ Main-Bot/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using NetCord.Gateway;
+using NetCord.Hosting.Gateway;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+if (builder.Configuration["Discord:Token"] == null)
+    throw new InvalidOperationException("Discord Token is not configured.");
+
+builder.Services
+    .AddDiscordGateway(options => { options.Intents = GatewayIntents.All; })
+    .AddGatewayHandlers(typeof(Program).Assembly);
+
+var host = builder.Build();
+
+await host.RunAsync();
---
+++ README.md
@@ -0,0 +1,13 @@
+# SN41 Discord
+
+## Main-Bot
+Environment options:
+
+| Option           | Needed | Description               | Default |
+|------------------|--------|---------------------------|---------|
+| `DISCORD__TOKEN` | ✅      | API Token used by the Bot |         |
+
+For development, you can use DotNet UserSecrets:
+```shell
+dotnet user-secrets set "Discord:Token" "your-actual-token-here"
+```
