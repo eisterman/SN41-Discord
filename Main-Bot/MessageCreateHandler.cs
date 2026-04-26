e9e6422a0f7c66b7443e0ebd077b7c43616ca830
Federico Pasqua <federico.pasqua.96@gmail.com>
2026-04-26 16:54:50 +0200

    Add AntiSpam to MessageCreateGateway

--- Main-Bot/MessageCreateHandler.cs
+++ Main-Bot/MessageCreateHandler.cs
@@ -1,18 +1,15 @@
-using Microsoft.Extensions.Logging;
 using NetCord.Gateway;
 using NetCord.Hosting.Gateway;
 using JetBrains.Annotations;
-using Microsoft.Extensions.Options;
+using Main_Bot.Features;
 
 namespace Main_Bot;
 
 [UsedImplicitly]
-public sealed class MessageCreateHandler(IOptions<SN41Settings> settings, ILogger<MessageCreateHandler> logger)
-    : IMessageCreateGatewayHandler {
-    public ValueTask HandleAsync(Message message) {
-        logger.LogInformation("{}", message.Content);
-        logger.LogInformation("Setted: {}",
-            string.Format(settings.Value.WelcomeMsgFeature.Body, message.Author.Username));
-        return default;
+public sealed class MessageCreateHandler(
+    AntiSpamLogicSingleton antiSpam
+) : IMessageCreateGatewayHandler {
+    public async ValueTask HandleAsync(Message message) {
+        await antiSpam.ProcessMessage(message);
     }
 }
